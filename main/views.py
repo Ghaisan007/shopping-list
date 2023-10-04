a5e7a6f4e0d4fa5f4676163be57f8feec70d48c6
Ghaisan007 <gl.aqila04@gmail.com>
2023-10-04 13:41:33 +0700

    Selesai mengerjakan Tutorial 5

--- main/views.py
+++ main/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseNotFound
 
 @login_required(login_url='/login')
 # Create your views here.
@@ -20,7 +22,7 @@ def show_main(request):
         'name': request.user.username,
         'class': 'PBP A',
         'products': products,
-        'last_login': request.COOKIES['last_login'],
+        'last_login': request.COOKIES.get('last_login'),
     }
 
     return render(request, "main.html", context)
@@ -114,3 +116,22 @@ def edit_product(request, id):
 
     context = {'form': form}
     return render(request, "edit_product.html", context)
+
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
